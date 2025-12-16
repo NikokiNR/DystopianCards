519c9a01dab65b7eb1fce522163d8ba7b7a7ee0e
nikokinr <nikokinr@gmail.com>
2025-12-16 09:01:27 -0300

    First example card

--- src/main/java/net/niko/dystopiancards/DystopianCards.java
+++ src/main/java/net/niko/dystopiancards/DystopianCards.java
@@ -2,6 +2,7 @@ package net.niko.dystopiancards;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.niko.dystopiancards.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class DystopianCards implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/niko/dystopiancards/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.niko.dystopiancards.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.niko.dystopiancards.DystopianCards;
+
+public class ModItems {
+
+    public static final Item CARD_BLANK = registerItem("card_blank", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(DystopianCards.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        DystopianCards.LOGGER.info("Registering Mod Items for " + DystopianCards.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+            entries.add(CARD_BLANK);
+        });
+    }
+}
